f1a064d8717e6742870191f0dbb8d55e551ac066
songmho <songmho@gmail.com>
2017-07-16 15:52:49 +0900

    0. 드로어 버튼을 눌러 여는 기능 추가 1. 드로어 내용 추가

--- app/src/main/java/com/momu/callrock/Activity/MainActivity.java
+++ app/src/main/java/com/momu/callrock/Activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.momu.callrock.Activity;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,9 @@ import com.momu.callrock.R;
 public class MainActivity extends AppCompatActivity {
 
     TextView btnSearch;
+    Button btnSetting;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnSearch = (TextView)findViewById(R.id.btn_search);
+        btnSetting = (Button)findViewById(R.id.btn_setting);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView)findViewById(R.id.navView);
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,SearchActivity.class));
+            }
+        });
+
+        btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.setDrawerListener(new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+
+                    }
+                });
+                drawerLayout.openDrawer(navigationView);
             }
         });
 
